2337ca59c735b49fad148574346fa770582e983e
lopes-leandro <lopesleandro.it@gmail.com>
2023-10-29 07:15:26 -0300

    refc: form from template AppProfileForm component

--- src/components/AppProfileForm.vue
+++ src/components/AppProfileForm.vue
@@ -1,7 +1,12 @@
 <template>
   <section
     class="md:w-2/3 flex flex-col p-12 items-center bg-red-200"
-  ></section>
+  >
+  <div class="flex flex-col">
+    <label for="name" class="flex text-gray-800 mb-2">Nome</label>
+    <input type="text" name="name" id="name" class="border-2 border-solid border-blue-200 rounded px-2 py-1">
+  </div>
+  </section>
 </template>
 <script setup lang="ts"></script>
 <style scoped></style>
