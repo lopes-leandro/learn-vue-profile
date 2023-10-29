11b35b33f5d76a855c197ad570acbaf6fb2b0b7e
lopes-leandro <lopesleandro.it@gmail.com>
2023-10-29 07:43:05 -0300

    refect: update form AppProfileForm

--- src/components/AppProfileForm.vue
+++ src/components/AppProfileForm.vue
@@ -1,12 +1,29 @@
 <template>
-  <section
-    class="md:w-2/3 flex flex-col p-12 items-center bg-red-200"
-  >
-  <div class="flex flex-col">
-    <label for="name" class="flex text-gray-800 mb-2">Nome</label>
-    <input type="text" name="name" id="name" class="border-2 border-solid border-blue-200 rounded px-2 py-1">
-  </div>
+  <section class="md:w-2/3 flex flex-col p-12 items-center bg-red-200">
+    <div class="flex flex-col">
+      <label for="name" class="flex text-gray-800 mb-2">Nome</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        class="border-2 border-solid border-blue-200 rounded px-2 py-1"
+        v-model="name"
+      />
+    </div>
+    <div class="flex flex-row mt-12">
+      <button type="submit" @click="formSubmit()">Enviar</button>
+    </div>
   </section>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const emit = defineEmits(["submit"]);
+const name = ref("");
+
+const formSubmit = () => {
+  emit("submit", {
+    name: name,
+  });
+}
+</script>
 <style scoped></style>
