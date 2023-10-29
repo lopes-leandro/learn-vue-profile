f4a9fc2cf787f1a9556d96085ebb18a0b4eb0208
lopes-leandro <lopesleandro.it@gmail.com>
2023-10-29 07:04:24 -0300

    Create component AppProfileForm

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import AppHeader from "./components/AppHeader.vue";
+import AppProfileFormVue from "./components/AppProfileForm.vue";
 </script>
 
 <template>
   <AppHeader />
+  <div>
+    <AppProfileFormVue />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppProfileForm.vue
@@ -0,0 +1,7 @@
+<template>
+  <section
+    class="md:w-2/3 flex flex-col p-12 items-center bg-red-200"
+  ></section>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
