42f3a64af22f95a24e7ad39164d8ecc03a05a38c
lopes-leandro <lopesleandro.it@gmail.com>
2023-10-29 07:58:05 -0300

    feat: create state management at app component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppProfileForm from "./components/AppProfileForm.vue";
 import AppProfileDisplay from "./components/AppProfileDisplay.vue";
+import { reactive } from "vue";
+
+const formData = reactive({name: ""});
+function update(e) {
+  formData.name = e.name;
+}
 </script>
 
 <template>
   <AppHeader />
   <div class="flex flex-col md:flex-row">
-    <AppProfileForm />
-    <AppProfileDisplay />
+    <AppProfileForm @submit="update($event)"/>
+    <AppProfileDisplay :form-data="formData"/>
   </div>
 </template>
 
--- src/components/AppProfileDisplay.vue
+++ src/components/AppProfileDisplay.vue
@@ -1,5 +1,9 @@
 <template>
-  <section class="md:w-1/3 h-64 bg-blue-200 flex"></section>
+  <section class="md:w-1/3 h-64 bg-blue-200 flex p-12">
+    <h3 class="font-bold font-lg">{{ formData.name }}</h3>
+  </section>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({formData: Object});
+</script>
 <style scoped></style>
